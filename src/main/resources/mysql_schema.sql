f4fc8c6026c8ce33999c2b3d0a63f4544925ade1
GiaKiet <nguyentrangiakiet47@gmail.com>
2024-05-14 14:37:46 +0700

    Add: schema and data for testing the project service

---
+++ src/main/resources/mysql_schema.sql
@@ -0,0 +1,39 @@
+create database sampleCollecting;
+use sampleCollecting;
+
+select * from user_account;
+select * from project;
+select * from sample;
+select * from sample_field;
+
+create table user_account (
+                              user_id varchar(100) not null primary key
+);
+
+create table project(
+                        id int not null primary key auto_increment,
+                        project_name varchar(200) not null,
+                        project_desc TINYTEXT default null,
+                        owner_id varchar(100) not null,
+                        foreign key (owner_id) references user_account(user_id)
+);
+
+create table sample(
+                       id int not null primary key auto_increment,
+                       image_path varchar(200) not null,
+                       project_id int not null,
+                       foreign key (project_id) references project(id)
+);
+
+create table sample_field(
+                             id int not null primary key auto_increment,
+                             field_name varchar(100) not null,
+                             feild_value TINYTEXT not null,
+                             sample_id int not null,
+                             foreign key (sample_id) references sample(id)
+);
+
+insert into user_account value ('bang123');
+insert into project value (1, 'testProjectName', 'testProjectDesc', 'bang123');
+insert into sample value (1, 'vsdfv/sfdbfdsb/agergerg', 1);
+insert into sample_field value (1, 'testFieldName', 'testFieldValue', 1);
